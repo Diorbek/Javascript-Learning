25913422f37ad77788dabe694731dc3195a71b8e
dior <diyorbekmutalipov@gmail.com>
2021-05-14 09:54:50 +0400

    add script file

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict" ;
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your lastname?', '');
+// answers[2] = prompt('how old are you?', '');
+
+// console.log(typeof(answer));
+// console.log(typeof(null));
+
+
+const category = "toys" ;
+// console.log('https://someurl.com/');
+// console.log('https://someurl.com/' + category + '/' +'4'); //concetenation
+console.log(`https://someurl.com/${category}/5`);
+
+
+const user = "dior";
+
+alert(`hello, ${user}`)
